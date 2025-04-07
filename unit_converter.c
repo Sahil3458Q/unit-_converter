c987433791b0a1c7de43da0fc907063325b532d1
Sahil3458Q <sahilshaikh13a@gmail.com>
2025-04-07 16:07:37 +0530

    Update unit_converter.c

--- unit_converter.c
+++ unit_converter.c
@@ -1,69 +1,96 @@
 #include<stdio.h>
+#include<stdlib.h>
 
-void kg_gm(float kg, float gm) {
-    if (gm == 0) {
-        printf("%.2f kg = %.2f gm\n", kg, kg * 1000.0);
-    } else if (kg == 0) {
-        printf("%.2f gm = %.2f kg\n", gm, gm / 1000.0);
-    } else {
-        printf("Please enter a valid value\n");
-    }
-}
-
-void km_m(float km, float m) {
-    if (m == 0) {
-        printf("%.2f km = %.2f m\n", km, km * 1000.0);
+void km_mi(float km, float mi) {
+    if (mi == 0) {
+        printf("%.2f km = %.2f mi\n", km, km * 0.621371);
     } else if (km == 0) {
-        printf("%.2f m = %.2f km\n", m, m / 1000.0);
+        printf("%.2f mi = %.2f km\n", mi, mi / 0.621371);
     } else {
         printf("Please enter a valid value\n");
     }
 }
-
-int main() {
-    float kg, m, gm, km ;
+void kg_lb(float kg, float lb) {
+    if (lb == 0) {
+        printf("%.2f kg = %.2f lb\n", kg, kg * 2.20462);
+    } else if (kg == 0) {
+        printf("%.2f lb = %.2f kg\n", lb, lb / 2.20462);
+    } else {
+        printf("Please enter a valid value\n");
+    }
+}
+void c_f(float c, float f) {
+    if (f == 0) {
+        printf("%.2f C = %.2f F\n", c, (c * 9.0 / 5.0) + 32);
+    } else if (c == 0) {
+        printf("%.2f F = %.2f C\n", f, (f - 32) * 5.0 / 9.0);
+    } else {
+        printf("Please enter a valid value\n");
+    }
+}
+int main(){
+    float kg, m, gm, km, mi, lb, c, f;
     int choice , i=1;
     printf("\n\n.....Welcome to the unit converter.....\n\n");
     for (i==1; i>0; i++){
-
-       
         printf("\nPlease Select the conversion you want to do : \n");
-        printf("1. kg to gm    2.gm to kg\n");
-        printf("3. km to m     4.m to km\n");
-        printf("5. exit\n");
+        printf("1. kg to lb       2.lb to kg\n");
+        printf("3. km to miles    4.miles to km\n");
+        printf("5. C to F         6.F to C\n");
+        printf("7. exit\n");
         printf("\nEnter your choice: ");
         scanf("%d", &choice);
         switch (choice) {
             case 1:
                 printf("\nEnter the value in KG: ");
                 scanf("%f", &kg); // Use %f for float
-                kg_gm(kg, 0);
+                kg_lb(kg, 0);
                 break;
             case 2:
-                printf("\nEnter the value in GM: ");
-                scanf("%f", &gm); // Use %f for float
-                kg_gm(0, gm);
+                printf("\nEnter the value in LB: ");
+                scanf("%f", &lb); // Use %f for float
+                kg_lb(0, lb);
                 break;
             case 3:
                 printf("\nEnter the value in KM: ");
                 scanf("%f", &km); // Use %f for float
-                km_m(km, 0);
+                km_mi(km, 0);
                 break;
             case 4:
-                printf("\nEnter the value in M: ");
-                scanf("%f", &m); // Use %f for float
-                km_m(0, m);
+                printf("\nEnter the value in MI: ");
+                scanf("%f", &mi); // Use %f for float
+                km_mi(0, mi);
                 break;
-
-
+            case 5:
+                printf("\nEnter the value in C: ");
+                scanf("%f", &c); // Use %f for float
+                c_f(c, 0);
+                break;
+            case 6:
+                printf("\nEnter the value in F: ");
+                scanf("%f", &f); // Use %f for float
+                c_f(0, f);
+                break;
         }
-        if (choice == 5) {
-            printf("Exiting the program.\n");
-            break;
+        if (choice == 7) {
+            printf("\nMade by Sahil\n");
+            exit(0);
         }
-
     }
-    
-
     return 0;
+
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
